2fb1190fc37ce61ea7e90a8d28fb1a233fcab548
Some_But!TOOL <143144389+fx2024ondiscord@users.noreply.github.com>
2025-01-01 08:37:18 -0800

    Delete Roblox-Key-System-main directory

--- Roblox-Key-System-main/Roblox-Key-System-main/Website/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'host' => 'remotemysql.com',
-    'username' => 'zvvqpzsMRN',
-    'password' => 'vk1pIO1Eje',
-    'database' => 'zvvqpzsMRN',
-    'recaptcha' => '6LdQCd4aAAAAAGDOR2sr6y_T_ZpFcERYvVVFV2k8'
-);
-?>
